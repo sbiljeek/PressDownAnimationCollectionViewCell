71bf7e0011271fc856789957bfa122ae7e9861f0
sbiljeek <sbiljeekdev@gmail.com>
2023-03-12 12:04:28 +0300

    Initial Commit

---
+++ PressDownAnimationCollectionViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PressDownAnimationCollectionViewCell
+//
+//  Created by Salman Biljeek on 3/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
